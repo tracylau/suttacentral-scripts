9f8ebaf396a6f54039c78f56fa6cdbb5f4f2938e
Tracy Lau <tljz@protonmail.com>
2023-09-23 23:39:53 +0800

    starting to update match-vb.py

--- pm-sanity/match-vb.py
+++ pm-sanity/match-vb.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
-## Set up filenames.
-bilara_data_path = "../../bilara-data"
+import os
+import subprocess
 
-bi_pm_file = f"{bilara_data_path}/translation/en/brahmali/vinaya/pli-tv-bi-pm_translation-en-brahmali.json"
-bu_pm_file = f"{bilara_data_path}/translation/en/brahmali/vinaya/pli-tv-bu-pm_translation-en-brahmali.json"
+## Set up directories and filenames.
+CWD = os.getcwd()
+VINAYA_PATH = "/Users/tracy/Development/bilara-data/translation/en/brahmali/vinaya"
+
+bi_pm_file = f"{VINAYA_PATH}/pli-tv-bi-pm_translation-en-brahmali.json"
+bu_pm_file = f"{VINAYA_PATH}/pli-tv-bu-pm_translation-en-brahmali.json"
 
 bi_pm_vb_segments_file = "pm-to-vb-bi.csv"
 bu_pm_vb_segments_file = "pm-to-vb-bu.csv"
+
+def isSegmentRange(text):
+    """
+    Given a string, returns True if the string contains a segment range.
+    Assumes this is formatted in "{from segment} - {to segment}"
+    """
+    return " - " in text
 
 def isSegmentId(text):
     """
@@ -25,12 +36,14 @@ def make_segment_id_dict(csv_file):
 
 def get_segment_text(segment_id):
     """
-    Return the translation text given a segment ID.  For vb segment IDs only.
-
-    Raises ValueError if segment ID is invalid.
-    Raises RuntimeError if segment cannot be found.
+    Return the translation text given a segment ID.
     """
-    return "foobar"
+    os.chdir(VINAYA_PATH)
+    output = subprocess.run(["ag", f'{segment_id}:', "--nofilename"], stdout=subprocess.PIPE).stdout.decode("utf-8").strip()
+    os.chdir(CWD)
+    # TODO decide whether to leave segment id bit in or not
+    # yeah...we need to get just the text, START HERE
+    return output
 
 def check_same_text(pm_file):
     """
@@ -50,9 +63,49 @@ def check_same_text(pm_file):
             check_line(line)
 
 def check_line(line):
+    """
+    For a content line in a pm file, check whether there is an equivalent segment
+    in the vb and complain if the text doesn't agree.  Or something like that.
+    """
     (segment_id, segment_text) = line.strip().rstrip(",").split(": ", 1)
     print(f"{segment_id}...{segment_text}")
 
+
+
+
+def compare(key_file):
+    """
+    For each line in the key csv file, check that the segments have the same text,
+    or if it is not a vb segment ID, check that the heading is in order.
+    """
+    with open(key_file) as csv_file:
+        for line in csv_file:
+            [pm_sid, vb_sid] = line.strip().split(",")
+            # Get the text for the pm entry.
+            # Sometimes the entry in the csv file is a range of segments rather
+            # than a single segment ID.  If it is a range, grab the segment texts
+            # and concatenate.
+            pm_stext = ""
+            if " - " in pm_sid:
+                [pm_sid_from, pm_sid_to] = pm_sid.split(" - ")
+                # Since the CSV data currently only has pm segment ranges of 2,
+                # we're just going to assume there are no segments in between
+                # the from and to segments.  We're not even going to write an
+                # assert (yet).
+                pm_stext = get_segment_text(pm_sid_from) + " " + get_segment_text(pm_sid_to)
+            else:
+                pm_stext = get_segment_text(pm_sid)
+
+            # Check whether the second entry is a segment ID.  If it isn't we're
+            # checking whether the rule title has the right number.
+
+            # Get the text for the segments.
+            # Sometimes the entry in the csv file is a range of segments rather
+            # than a single segment ID.  If it is a range, grab the segment texts
+            # and concatenate.
+
+            print(f"{pm_sid} ... {pm_stext}")
+
 # Run!
-check_same_text(bi_pm_file)
-check_same_text(bu_pm_file)
+compare(bu_pm_vb_segments_file)
+#check_same_text(bu_pm_file)
